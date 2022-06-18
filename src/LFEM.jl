259b5d9fc54be1b0c7871a9073374ee91f344230
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-18 14:06:55 -0400

    Include driver and export Stress

--- src/LFEM.jl
+++ src/LFEM.jl
@@ -6,10 +6,12 @@ include("load.jl")
 include("truss2D.jl")
 include("truss3D.jl")
 include("global.jl")
+include("drivers.jl")
 
 export Point_load
 export K_truss2D, B_truss2D, Stress_truss2D
 export K_truss3D, B_truss3D, Stress_truss3D
 export Global_K, Solve_KU
+export Stress
 
 end # module
