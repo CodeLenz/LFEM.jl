ff4fa3cdc9adf96e69b8d1bdacd14bb101bec393
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-18 14:00:54 -0400

    Stress

--- src/truss2D.jl
+++ src/truss2D.jl
@@ -8,3 +8,40 @@ function K_truss2D(E,A,L)
                  0.0 0.0  0.0 0.0 ]
 end
         
+#
+# Local B matrix
+#
+function B_truss2D(L)
+       [-1/L 0.0 1/L 0.0]
+end
+
+#
+# Local stress
+#
+function Stress_truss2D(mesh::Mesh2D,ele::Int64,U::Vector{Float64};xe=1.0,p=1.0,q=0.0)
+
+       # Descobre os dados do elemento
+       Ee = mesh.materials[1].Ex
+       Ae = mesh.geometries[1].A
+       Le = Length(bmesh,ele)
+
+       # Matriz B do elemento
+       B = B_truss2D(Le)
+
+       # Monta a matriz de rigidez local
+       Ke = K_truss2D(Ee,Ae,Le)
+
+       # Matriz de rotação
+       Te = T_matrix(bmesh,ele)
+
+       # Global DOFs
+       gls = DOFs(bmesh,ele) 
+
+       # Local displacements
+       u = Te*U[gls]
+
+       # Stress
+       (xe^(p-q))*Ee*B*u
+       
+
+end
