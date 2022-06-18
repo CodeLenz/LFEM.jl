eef3d1b4e9dbfbca67350d04af3a06446545fc86
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-18 09:16:44 -0400

    Make x and p optional

--- src/global.jl
+++ src/global.jl
@@ -1,11 +1,19 @@
-function Global_K(mesh::Mesh, x::Vector{Float64}; p::Float64)
+function Global_K(mesh::Mesh; x=Float64[]; p=1.0)
 
     # Alias
     bmesh = mesh.bmesh
+    ne = bmesh.ne
 
+    # Basic test for x
+    if isempty(x)
+        x = ones(ne)
+    end
+    
     # Vamos reforçar a ideia de que x tem a dimensão ne
     @assert length(x)==bmesh.ne "Global_K::x deve ter dimensão igual o número de elementos"
 
+    p>=1.0 || throw("Global_K:::p must be larger or equal to 1.0")
+    
     # Dimensão do problema
     dim = 2
     if isa(mesh,Mesh3D)
@@ -24,7 +32,7 @@ function Global_K(mesh::Mesh, x::Vector{Float64}; p::Float64)
 
     # Loop pelos elementos, calculando a matriz local Ke de cada um
     # e posicionando na K
-    for ele=1:bmesh.ne
+    for ele=1:ne
 
         # Descobre os dados do elemento
         Ee = mesh.materials[1].Ex
@@ -70,10 +78,10 @@ end
 #
 # Sequencia de chamadas para a solução do problema de equilíbrio
 #
-function Solve_KU(mesh::Mesh, x::Vector{Float64}; p)
+function Solve_KU(mesh::Mesh; x=Float64[], p=1.0)
   
     # Assembly
-    K = Global_K(mesh,x;p=p)
+    K = Global_K(mesh;x=x,p=p)
     F = Point_load(mesh)
 
     # Free dofs
