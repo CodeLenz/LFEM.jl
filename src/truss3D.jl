fb90a38647400e55ee44e62676cb1a1338277fa4
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-18 14:25:30 -0400

    Fixing access to bmesh

--- src/truss3D.jl
+++ src/truss3D.jl
@@ -24,7 +24,7 @@ function Stress_truss3D(mesh::Mesh3D,ele::Int64,U::Vector{Float64};xe=1.0,p=1.0,
     # Descobre os dados do elemento
     Ee = mesh.materials[1].Ex
     Ae = mesh.geometries[1].A
-    Le = Length(bmesh,ele)
+    Le = Length(mesh.bmesh,ele)
 
     # Matriz B do elemento
     B = B_truss3D(Le)
@@ -33,10 +33,10 @@ function Stress_truss3D(mesh::Mesh3D,ele::Int64,U::Vector{Float64};xe=1.0,p=1.0,
     Ke = K_truss3D(Ee,Ae,Le)
 
     # Matriz de rotação
-    Te = T_matrix(bmesh,ele)
+    Te = T_matrix(mesh.bmesh,ele)
 
     # Global DOFs
-    gls = DOFs(bmesh,ele) 
+    gls = DOFs(mesh.bmesh,ele) 
 
     # Local displacements
     u = Te*U[gls]
