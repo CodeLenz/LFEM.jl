ded8a955387c754107f1127b6a4ad3279013d988
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-21 11:26:09 -0300

    Update LFEM.jl

--- src/LFEM.jl
+++ src/LFEM.jl
@@ -8,6 +8,7 @@ include("truss3D.jl")
 include("global.jl")
 include("drivers.jl")
 include("show.jl")
+include("gmsh.jl")
 
 export Point_load
 export K_truss2D, B_truss2D, Stress_truss2D
@@ -15,5 +16,6 @@ export K_truss3D, B_truss3D, Stress_truss3D
 export Global_K, Solve_KU
 export Stress
 export plot
+export Gmsh_init, Gmsh_nodal_scalar, Gmsh_element_scalar, Gmsh_nodal_vector
 
 end # module
