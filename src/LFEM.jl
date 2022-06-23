cc495234e65aeb57851961dd752472410001abde
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-23 18:48:17 -0400

    Include base.jl
    Add Expand_vector(!)

--- src/LFEM.jl
+++ src/LFEM.jl
@@ -5,6 +5,7 @@ using Plots, Arpack
 using BMesh, LMesh
 
 include("load.jl")
+include("base.jl")
 include("elements/truss2D.jl")
 include("elements/truss3D.jl")
 include("global.jl")
@@ -15,6 +16,7 @@ include("show.jl")
 include("gmsh.jl")
 
 export Point_load
+export Expand_vector, Expand_vector!
 export K_truss2D, M_truss2D, B_truss2D, Stress_truss2D
 export K_truss3D, M_truss3D, B_truss3D, Stress_truss3D
 export Global_K, Global_M
