a63955d74faf3e467aeab488228a596f7638d6b6
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-23 11:02:48 -0300

    Exporting M_trussXD

--- src/LFEM.jl
+++ src/LFEM.jl
@@ -12,8 +12,8 @@ include("show.jl")
 include("gmsh.jl")
 
 export Point_load
-export K_truss2D, B_truss2D, Stress_truss2D
-export K_truss3D, B_truss3D, Stress_truss3D
+export K_truss2D, M_truss2D, B_truss2D, Stress_truss2D
+export K_truss3D, M_truss3D, B_truss3D, Stress_truss3D
 export Global_K, Global_M
 export Solve_KU
 export Stress, Keg_truss, Meg_truss
