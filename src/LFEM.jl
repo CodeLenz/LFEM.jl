0a752f0c412c69b3c78efd18aa8bfc8f1d36cd99
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-23 09:55:34 -0400

    Update new file structure
    and export new functions associated to local and global mass matrices

--- src/LFEM.jl
+++ src/LFEM.jl
@@ -3,18 +3,20 @@ module LFEM
 using LinearAlgebra, SparseArrays, BMesh, LMesh, Plots
 
 include("load.jl")
-include("truss2D.jl")
-include("truss3D.jl")
+include("elements/truss2D.jl")
+include("elements/truss3D.jl")
 include("global.jl")
 include("drivers.jl")
+include("analysis/linear.jl")
 include("show.jl")
 include("gmsh.jl")
 
 export Point_load
 export K_truss2D, B_truss2D, Stress_truss2D
 export K_truss3D, B_truss3D, Stress_truss3D
-export Global_K, Solve_KU
-export Stress, Keg_truss
+export Global_K, Global_M
+export Solve_KU
+export Stress, Keg_truss, Meg_truss
 export plot
 export Gmsh_init, Gmsh_nodal_scalar, Gmsh_element_scalar, Gmsh_nodal_vector
 
