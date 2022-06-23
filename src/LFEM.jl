af4d60ef4f6f34f5375e50796b099b21a73bc06a
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-23 13:33:29 -0400

    Update to consider Arpack and export modal analysis

--- src/LFEM.jl
+++ src/LFEM.jl
@@ -1,6 +1,8 @@
 module LFEM
 
-using LinearAlgebra, SparseArrays, BMesh, LMesh, Plots
+using LinearAlgebra, SparseArrays
+using Plots, Arpack
+using BMesh, LMesh
 
 include("load.jl")
 include("elements/truss2D.jl")
@@ -8,6 +10,7 @@ include("elements/truss3D.jl")
 include("global.jl")
 include("drivers.jl")
 include("analysis/linear.jl")
+include("analysis/modal.jl")
 include("show.jl")
 include("gmsh.jl")
 
@@ -15,7 +18,7 @@ export Point_load
 export K_truss2D, M_truss2D, B_truss2D, Stress_truss2D
 export K_truss3D, M_truss3D, B_truss3D, Stress_truss3D
 export Global_K, Global_M
-export Solve_KU
+export Solve_KU, Solve_Modal
 export Stress, Keg_truss, Meg_truss
 export plot
 export Gmsh_init, Gmsh_nodal_scalar, Gmsh_element_scalar, Gmsh_nodal_vector
