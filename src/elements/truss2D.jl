7de144caf0ad7b8d7d8816c1548c0483ece58888
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-23 10:50:44 -0300

    Create truss2D.jl
    Move file to elements/
    add local mass matrix (lumped)

---
+++ src/elements/truss2D.jl
@@ -0,0 +1,54 @@
+#
+# Local stiffness matrix
+#
+function K_truss2D(E,A,L)
+       (E*A/L)*[ 1.0 0.0 -1.0 0.0 ;
+                 0.0 0.0  0.0 0.0 ; 
+                -1.0 0.0  1.0 0.0 ;
+                 0.0 0.0  0.0 0.0 ]
+end
+  
+#
+# Local mass matrix (Lumped)
+#
+function M_truss2D(dens,A,L,lumped=true)
+        (dens*A*L/2)*[ 1.0 0.0  0.0 0.0 ;
+                       0.0 1.0  0.0 0.0 ; 
+                       0.0 0.0  1.0 0.0 ;
+                       0.0 0.0  0.0 1.0 ]
+end
+
+
+#
+# Local B matrix
+#
+function B_truss2D(L)
+       [-1/L 0.0 1/L 0.0]
+end
+
+#
+# Local stress
+#
+function Stress_truss2D(mesh::Mesh2D,ele::Int64,U::Vector{Float64};xe=1.0,p=1.0,q=0.0)
+
+       # Descobre os dados do elemento
+       Ee = mesh.materials[1].Ex
+       Le = Length(mesh.bmesh,ele)
+
+       # Matriz B do elemento
+       B = B_truss2D(Le)
+
+       # Matriz de rotação
+       Te = T_matrix(mesh.bmesh,ele)
+
+       # Global DOFs
+       gls = DOFs(mesh.bmesh,ele) 
+
+       # Local displacements
+       u = Te*U[gls]
+
+       # Stress
+       (xe^(p-q))*Ee*B*u
+       
+
+end
