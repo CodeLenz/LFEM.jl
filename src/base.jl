cd59cf2f8872a548c0d70bad363878f2b2ef2681
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-23 18:47:11 -0400

    Create base.jl

---
+++ src/base.jl
@@ -0,0 +1,21 @@
+#
+# Copy v to V[free_dofs] inplace
+#
+function Expand_vector!(V::Vector{T},v::Vector{T},free_dofs::Vector{Int64}) where {T}
+
+    # Basic assertions
+    length(v)==length(free_dofs)||throw("Expand_vector!:: v and free_dofs must have the same size")
+    length(V)>=length(free_dofs)||throw("Expand_vector!:: length of V must be larger than the length of v")
+
+    # Copy
+    V[free_dofs].=v
+end
+
+
+function Expand_vector(v::Vector{T},dim::Int64,free_dofs::Vector{Int65}) where {T}
+
+    V = zeros(T,dim)
+    Expand!(V,v,free_dofs)
+    return V
+
+end
