8cdf21769b7b13b93c262a6be34b1f7b25e185ce
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-24 07:49:53 -0400

    Harmonic analysis

---
+++ src/analysis/harmonic.jl
@@ -0,0 +1,41 @@
+#
+# Sequencia de chamadas para a solução do problema harmônico
+#
+function Solve_modal(mesh::Mesh, w::Float64 ; x=Float64[], p=1.0)
+  
+    # Basic check
+    w >= 0.0 || throw("Solve_modal:: angular frequency w must be positive")
+
+    # Assembly K and M
+    K = Global_K(mesh;x=x,p=p)
+    M = Global_M(mesh;x=x)
+
+    # Assembly F
+    F = Point_load(mesh)
+
+    # Free dofs
+    free_dofs = mesh.free_dofs
+    
+    # Local views to the free dofs
+    KV = @view  K[free_dofs, free_dofs]
+    MV = @view  M[free_dofs, free_dofs]
+
+    # Local damping
+    CV = 1E-6*KV
+
+    # Harmonic matrix
+    KD = KV + w*im*CV - (w^2)*MV
+
+    # Lu decomposition
+    LU = lu(KD)
+    
+    # Harmonic displacement
+    Ul = LU\F[free_dofs]
+
+    # Expand 
+    Ud = Expand!(Ul,free_dofs)
+    
+    # Return
+    return Ud, LU
+    
+ end
