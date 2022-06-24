143e392e9b14f14c81be10d0506a5b9508697c9a
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-24 08:51:00 -0300

    Add Solve_harmonic

--- src/LFEM.jl
+++ src/LFEM.jl
@@ -12,6 +12,7 @@ include("global.jl")
 include("drivers.jl")
 include("analysis/linear.jl")
 include("analysis/modal.jl")
+include("analysis/harmonic.jl")
 include("show.jl")
 include("gmsh.jl")
 
@@ -20,7 +21,7 @@ export Expand_vector, Expand_vector!
 export K_truss2D, M_truss2D, B_truss2D, Stress_truss2D
 export K_truss3D, M_truss3D, B_truss3D, Stress_truss3D
 export Global_K, Global_M
-export Solve_linear, Solve_modal
+export Solve_linear, Solve_modal, Solve_harmonic
 export Stress, Keg_truss, Meg_truss
 export plot
 export Gmsh_init, Gmsh_nodal_scalar, Gmsh_element_scalar, Gmsh_nodal_vector
