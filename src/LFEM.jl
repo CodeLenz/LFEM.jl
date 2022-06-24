2076fec4cba9ae18a4e803e3d116706d08ce796c
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-24 11:34:35 -0300

    Update LFEM.jl

--- src/LFEM.jl
+++ src/LFEM.jl
@@ -13,6 +13,7 @@ include("drivers.jl")
 include("analysis/linear.jl")
 include("analysis/modal.jl")
 include("analysis/harmonic.jl")
+include("analysis/newmark.jl")
 include("show.jl")
 include("gmsh.jl")
 
@@ -21,7 +22,7 @@ export Expand_vector, Expand_vector!
 export K_truss2D, M_truss2D, B_truss2D, Stress_truss2D
 export K_truss3D, M_truss3D, B_truss3D, Stress_truss3D
 export Global_K, Global_M
-export Solve_linear, Solve_modal, Solve_harmonic
+export Solve_linear, Solve_modal, Solve_harmonic, Solve_newmark
 export Stress, Keg_truss, Meg_truss
 export plot
 export Gmsh_init, Gmsh_nodal_scalar, Gmsh_element_scalar, Gmsh_nodal_vector
