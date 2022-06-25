572fa11d27050a5ecf6450e04c485881331bdf9d
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-25 09:15:54 -0400

    Use mat_ele

--- src/elements/truss2D.jl
+++ src/elements/truss2D.jl
@@ -32,7 +32,8 @@ end
 function Stress_truss2D(mesh::Mesh2D,ele::Int64,U::Vector{Float64};xe=1.0,p=1.0,q=0.0)
 
        # Descobre os dados do elemento
-       Ee = mesh.materials[1].Ex
+       mat = mesh.mat_ele[ele]
+       Ee = mesh.materials[mat].Ex
        Le = Length(mesh.bmesh,ele)
 
        # Matriz B do elemento
