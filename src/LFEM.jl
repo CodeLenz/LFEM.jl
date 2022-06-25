e466ef89f4c7e82ce381efff5cb14d861b4a5107
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-25 15:29:03 -0400

    Add Solid2D and new functions

--- src/LFEM.jl
+++ src/LFEM.jl
@@ -8,6 +8,7 @@ include("load.jl")
 include("base.jl")
 include("elements/truss2D.jl")
 include("elements/truss3D.jl")
+include("elements/solid2D.jl")
 include("global.jl")
 include("drivers.jl")
 include("analysis/linear.jl")
@@ -21,9 +22,10 @@ export Point_load
 export Expand_vector, Expand_vector!
 export K_truss2D, M_truss2D, B_truss2D, Stress_truss2D
 export K_truss3D, M_truss3D, B_truss3D, Stress_truss3D
+export K_solid2D, M_solid2D
 export Global_K, Global_M
 export Solve_linear, Solve_modal, Solve_harmonic, Solve_newmark
-export Stress, Keg_truss, Meg_truss
+export Stress, Keg_truss, Meg_truss, Keg_solid, Meg_solid
 export plot
 export Gmsh_init, Gmsh_nodal_scalar, Gmsh_element_scalar, Gmsh_nodal_vector
 
