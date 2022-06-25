7b4b2479e063ad1b4fad678682a55d852a24e46a
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-25 15:40:05 -0400

    Add tests to Nodal_coordinates (2D)

--- test/test_base.jl
+++ test/test_base.jl
@@ -28,7 +28,95 @@
   pos2 = [1;2]
   @test_throws String Expand_vector(u,10,pos2)
   
-  
-  
+  #
+  # Nodal coordinates
+  #
+  # Nodal_coordinates(m::Mesh2D,ele)
+
+    ####################  2D BMesh ######################
+    # Single bar in tension (L=0.1m F=100.0 A=1.0 E=100.0)
+    #
+    #    Valid inputs (no error) 
+    #
+    nn = 2
+    ne = 1
+    coord = [0.0 0.0 ;
+             0.1 0.0 ]
+    connect = [1 2 ]
+    Lx = 1.0
+    Ly = 1.0
+    nx = 1
+    ny = 1
+    etype = :truss2D
+
+    # truss2D
+    b2 = Bmesh2D(etype,nn,ne,coord,connect,Lx,Ly,nx,ny)
+
+    # Essential boundary conditions
+    ebc = [1 1 0.0 ; 
+           1 2 0.0 ; 
+           2 2 0.0 ]
+
+    # Natural boundary conditions
+    nbc = [2 1 100.0]
+
+    # Material and geometry
+    materials = [Material(Ex=100.0,density=1.0)]
+    geometries = [Geometry(A=1.0)]
+           
+    # Mesh
+    m2 = Mesh2D(b2,materials,geometries,ebc,nbc)
+
+    # Nodal coordinates of element 1 
+    x,y=Nodal_coordinates(m2,1)
+    xref = [0.0;0.1]
+    yref = [0.0;0.0]
+    @assert all(x.==xref)
+    @assert all(y.==yref)
+
+
+    ####################  2D BMesh ######################
+    # Square
+    #
+    #    Valid inputs (no error) 
+    #
+    nn = 4
+    ne = 1
+    coord = [0.0 0.0 ;
+             1.0 0.0 ;
+             1.1 1.1 ;
+             0.0 1.0]
+    connect = [1 2 3 4]
+    Lx = 1.0
+    Ly = 1.0
+    nx = 1
+    ny = 1
+    etype = :solid2D
+
+    # truss2D
+    b2 = Bmesh2D(etype,nn,ne,coord,connect,Lx,Ly,nx,ny)
+
+    # Essential boundary conditions
+    ebc = [1 1 0.0 ; 
+           1 2 0.0 ; 
+           2 1 0.0 ]
+
+    # Natural boundary conditions
+    nbc = [3 1 100.0]
+
+    # Material and geometry
+    materials = [Material(Ex=100.0,density=1.0)]
+    geometries = [Geometry(thickness=1.0)]
+           
+    # Mesh
+    m2 = Mesh2D(b2,materials,geometries,ebc,nbc)
+
+    # Nodal coordinates of element 1 
+    x,y=Nodal_coordinates(m2,1)
+    xref = [0.0;1.0;1.1;0.0]
+    yref = [0.0;0.0;1.1;1.0]
+    @assert all(x.==xref)
+    @assert all(y.==yref)
+
   
 end
