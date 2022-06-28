a36a48138c48b0c0e93888ae48ce2e958edaed0d
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-28 07:15:37 -0400

    Converting to use StaticArrays

--- src/elements/truss2D.jl
+++ src/elements/truss2D.jl
@@ -2,20 +2,20 @@
 # Local stiffness matrix
 #
 function K_truss2D(E,A,L)
-       (E*A/L)*[ 1.0 0.0 -1.0 0.0 ;
-                 0.0 0.0  0.0 0.0 ; 
-                -1.0 0.0  1.0 0.0 ;
-                 0.0 0.0  0.0 0.0 ]
+      SMatrix{4,4,Float64}( (E*A/L)*[ 1.0 0.0 -1.0 0.0 ;
+                                      0.0 0.0  0.0 0.0 ; 
+                                     -1.0 0.0  1.0 0.0 ;
+                                      0.0 0.0  0.0 0.0 ] )
 end
   
 #
 # Local mass matrix (Lumped)
 #
 function M_truss2D(dens,A,L,lumped=true)
-        (dens*A*L/2)*[ 1.0 0.0  0.0 0.0 ;
-                       0.0 1.0  0.0 0.0 ; 
-                       0.0 0.0  1.0 0.0 ;
-                       0.0 0.0  0.0 1.0 ]
+        SMatrix{4,4,Float64}((dens*A*L/2)*[ 1.0 0.0  0.0 0.0 ;
+                                            0.0 1.0  0.0 0.0 ; 
+                                            0.0 0.0  1.0 0.0 ;
+                                            0.0 0.0  0.0 1.0 ] )
 end
 
 
@@ -23,7 +23,7 @@ end
 # Local B matrix
 #
 function B_truss2D(L)
-       [-1/L 0.0 1/L 0.0]
+       SMatrix{1,4,Float64}( [-1/L 0.0 1/L 0.0] )
 end
 
 #
@@ -45,8 +45,11 @@ function Stress_truss2D(mesh::Mesh2D,ele::Int64,U::Vector{Float64};xe=1.0,p=1.0,
        # Global DOFs
        gls = DOFs(mesh.bmesh,ele) 
 
-       # Local displacements
-       u = Te*U[gls]
+       # Element displacements in global reference
+       ug = @SVector{4,Float64}(U[gls])
+       
+       # Element displacements in local reference
+       u = Te*ug
 
        # Stress
        (xe^(p-q))*Ee*B*u
