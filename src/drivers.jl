42db90f4926136a2c664d45b9f7b454a0ca1539b
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-07-01 12:53:41 -0300

    Major refactor
    1) Stress for solid elements. Split driver for 2D and 3D and make a default (0.0,0.0,0.0) version
    2) Split Local_K and Local_M for 2D and 3D (automatic)

--- src/drivers.jl
+++ src/drivers.jl
@@ -1,24 +1,55 @@
 #
-# Driver for stress
+# Driver for stress (2D)
 #
-function Stress(mesh::Mesh,ele::Int64,U::Vector{Float64};xe=1.0,p=1.0,q=0.0)
+function Stress(r::Float64,s::Float64,mesh::Mesh2D,ele::Int64,U::Vector{Float64};xe=1.0,p=1.0,q=0.0)
   
    etype = Get_etype(mesh)
   
    if etype==:truss2D
          return  Stress_truss2D(mesh,ele,U;xe=xe,p=p,q=q)  
-   elseif etype==:truss3D
-       return  Stress_truss3D(mesh,ele,U;xe=xe,p=p,q=q)  
+   elseif etype==:solid2D
+       return  Stress_solid2D(r,s,mesh,ele,U;xe=xe,p=p,q=q)  
    else
        error("Stress:: element $etype:: not implemented")
-  end
+   end
   
 end
-    
+
+# Default  (r=s=0.0)
+function Stress(mesh::Mesh2D,ele::Int64,U::Vector{Float64};xe=1.0,p=1.0,q=0.0)
+   Stress(0.0,0.0,mesh,ele,U;xe=xe,p=p,q)
+end
+
+
 #
-# Driver for Stiffness
+# Driver for stress (3D)
 #
-function Local_K(mesh::Mesh,ele::Int64)
+function Stress(r::Float64,s::Float64,t::Float64,
+                mesh::Mesh3D,ele::Int64,U::Vector{Float64};xe=1.0,p=1.0,q=0.0)
+  
+   etype = Get_etype(mesh)
+  
+   if etype==:truss3D
+       return  Stress_truss3D(mesh,ele,U;xe=xe,p=p,q=q)  
+   elseif etype==:solid3D
+       return  Stress_solid3D(r,s,t,mesh,ele,U;xe=xe,p=p,q=q)  
+   else
+       error("Stress:: element $etype:: not implemented")
+   end
+  
+end
+
+
+# Default  (r=s=t=0.0)
+function Stress(mesh::Mesh3D,ele::Int64,U::Vector{Float64};xe=1.0,p=1.0,q=0.0)
+   Stress(0.0,0.0,0.0,mesh,ele,U;xe=xe,p=p,q)
+end
+
+
+#
+# Driver for Stiffness (2D)
+#
+function Local_K(mesh::Mesh2D,ele::Int64)
 
    # Element type
    etype = Get_etype(mesh)
@@ -28,13 +59,32 @@ function Local_K(mesh::Mesh,ele::Int64)
 
       Ke = K_solid2D(mesh,ele)
 
-   elseif etype==:solid3D
-
-      Ke = K_solid3D(mesh,ele)   
-
    elseif etype==:truss2D
 
       Ke = K_truss2D(mesh,ele)
+
+   else
+      
+      throw("Local_K:: etype $etype is invalid")
+
+   end
+   
+   return Ke
+
+end
+
+#
+# Driver for Stiffness (3D)
+#
+function Local_K(mesh::Mesh3D,ele::Int64)
+
+   # Element type
+   etype = Get_etype(mesh)
+  
+   # If solid
+   if etype==:solid3D 
+
+      Ke = K_solid3D(mesh,ele)
 
    elseif etype==:truss3D  
       
@@ -51,10 +101,11 @@ function Local_K(mesh::Mesh,ele::Int64)
 end
 
 
+
 #
-# Driver for Mass
+# Driver for Mass (2D)
 #
-function Local_M(mesh::Mesh,ele::Int64)
+function Local_M(mesh::Mesh2D,ele::Int64)
 
    # Element type
    etype = Get_etype(mesh)
@@ -64,13 +115,33 @@ function Local_M(mesh::Mesh,ele::Int64)
 
       Me = M_solid2D(mesh,ele)
 
-   elseif etype==:solid3D
-
-      Me = M_solid3D(mesh,ele)   
-
    elseif etype==:truss2D
 
       Me = M_truss2D(mesh,ele)
+
+   else
+      
+      throw("Local_M:: etype $etype is invalid")
+
+   end
+   
+   return Me
+
+end
+
+
+#
+# Driver for Mass (3D)
+#
+function Local_M(mesh::Mesh3D,ele::Int64)
+
+   # Element type
+   etype = Get_etype(mesh)
+  
+   # If solid
+   if etype==:solid3D 
+
+      Me = M_solid3D(mesh,ele)
 
    elseif etype==:truss3D  
       
