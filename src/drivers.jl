31d3181e72f71b224c41bae65568591ae48129ab
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-07-02 10:48:31 -0300

    Major refactor
    Add docstrings
    Change material parametrization

--- src/drivers.jl
+++ src/drivers.jl
@@ -1,14 +1,14 @@
 #
 # Driver for stress (2D)
 #
-function Stress(r::Float64,s::Float64,mesh::Mesh2D,ele::Int64,U::Vector{Float64};xe=1.0,p=1.0,q=0.0)
+function Stress(r::Float64,s::Float64,mesh::Mesh2D,ele::Int64,U::Vector{Float64})
   
    etype = Get_etype(mesh)
   
    if etype==:truss2D
-         return  Stress_truss2D(mesh,ele,U;xe=xe,p=p,q=q)  
+         return  Stress_truss2D(mesh,ele,U)  
    elseif etype==:solid2D
-       return  Stress_solid2D(r,s,mesh,ele,U;xe=xe,p=p,q=q)  
+       return  Stress_solid2D(r,s,mesh,ele,U)  
    else
        error("Stress:: element $etype:: not implemented")
    end
@@ -16,8 +16,8 @@ function Stress(r::Float64,s::Float64,mesh::Mesh2D,ele::Int64,U::Vector{Float64}
 end
 
 # Default  (r=s=0.0)
-function Stress(mesh::Mesh2D,ele::Int64,U::Vector{Float64};xe=1.0,p=1.0,q=0.0)
-   Stress(0.0,0.0,mesh,ele,U;xe=xe,p=p,q)
+function Stress(mesh::Mesh2D,ele::Int64,U::Vector{Float64})
+   Stress(0.0,0.0,mesh,ele,U)
 end
 
 
@@ -25,14 +25,14 @@ end
 # Driver for stress (3D)
 #
 function Stress(r::Float64,s::Float64,t::Float64,
-                mesh::Mesh3D,ele::Int64,U::Vector{Float64};xe=1.0,p=1.0,q=0.0)
+                mesh::Mesh3D,ele::Int64,U::Vector{Float64})
   
    etype = Get_etype(mesh)
   
    if etype==:truss3D
-       return  Stress_truss3D(mesh,ele,U;xe=xe,p=p,q=q)  
+       return  Stress_truss3D(mesh,ele,U)  
    elseif etype==:solid3D
-       return  Stress_solid3D(r,s,t,mesh,ele,U;xe=xe,p=p,q=q)  
+       return  Stress_solid3D(r,s,t,mesh,ele,U)  
    else
        error("Stress:: element $etype:: not implemented")
    end
@@ -41,8 +41,8 @@ end
 
 
 # Default  (r=s=t=0.0)
-function Stress(mesh::Mesh3D,ele::Int64,U::Vector{Float64};xe=1.0,p=1.0,q=0.0)
-   Stress(0.0,0.0,0.0,mesh,ele,U;xe=xe,p=p,q)
+function Stress(mesh::Mesh3D,ele::Int64,U::Vector{Float64})
+   Stress(0.0,0.0,0.0,mesh,ele,U)
 end
 
 
@@ -158,53 +158,4 @@ function Local_M(mesh::Mesh3D,ele::Int64)
 end
 
 
-
-#
-# Evaluate stresses for the entire mesh (central points only)
-#
-"""
-Return stresses for the entire mesh. This version evaluates only in the central point.
-
-  Stresses(mesh::Mesh,U::Vector{T};x=Float64[],p=1.0,q=0.0)
-
-The output is a matrix ne x ncol, where ncol is 1 for :truss2D and 3D, 3 for :solid2D and 6 for :solid3D
-
-
-"""
-function Stresses(mesh::Mesh,U::Vector{T};x=Float64[],p=1.0,q=0.0)  where T
-
-   # Number of elements
-   ne = Get_ne(mesh)
-
-   # Check for empty x
-   if isempty(x)
-      x1 = ones(ne)
-   else
-      x1=x
-   end   
-
-   # Alocate the output array. It depends on the number of stresses 
-   # for each element type
-   etype = Get_etype(mesh)
-
-   ncol = 1
-   if etype==:solid2D
-      ncol = 3
-   elseif etype==:solid3D
-      ncol = 6
-   end
-
-   # Alocate
-   stresses = zeros(ne,ncol)
-
-   # Loop 
-   for ele in mesh
-      s = Stress(mesh,ele,U,xe=x1[ele],p=p,q=q)
-      stresses[ele,:].=s[:]
-   end
-
-   # Return stresses
-   return stresses
-
-end
 
--- src/global.jl
+++ src/global.jl
@@ -1,19 +1,32 @@
-function Global_K(mesh::Mesh; x=Float64[], p=1.0)
+"""
+Assembly the global stiffness matrix.
+
+    Global_K(mesh::Mesh, x::Vector{Float64}, kparam::Function)
+
+where kparam(x) can be, for example
+
+function kparam(x,p=1.0)
+    x^p
+end
+
+for a SIMP like material parametrization.
+"""
+function Global_K(mesh::Mesh, xin::Vector{Float64}, kparam::Function)
 
     # Alias
-    bmesh = mesh.bmesh
-    ne = bmesh.ne
+    ne = Get_ne(mesh)
+    nn = Get_nn(mesh)
 
-    # Basic test for x
-    if isempty(x)
+     # Basic test for xe
+     if isempty(xin)
         x = ones(ne)
+    else
+        x = copy(xin)
     end
-    
-    # Vamos reforçar a ideia de que x tem a dimensão ne
-    @assert length(x)==bmesh.ne "Global_K::x deve ter dimensão igual o número de elementos"
 
-    p>=1.0 || throw("Global_K:::p must be larger or equal to 1.0")
-    
+    # Vamos reforçar a ideia de que x tem a dimensão ne
+    @assert length(x)==ne "Global_K::x deve ter dimensão igual o número de elementos"
+
     # Dimensão do problema
     dim = Get_dim(mesh)
     
@@ -21,7 +34,7 @@ function Global_K(mesh::Mesh; x=Float64[], p=1.0)
     etype = Get_etype(mesh)
 
     # Primeira coisa é alocar a matriz K 
-    ng = dim*bmesh.nn
+    ng = dim*nn
     K = spzeros(ng,ng)
 
     # Loop pelos elementos, calculando a matriz local Ke de cada um
@@ -39,7 +52,7 @@ function Global_K(mesh::Mesh; x=Float64[], p=1.0)
         Keg = To_global(Ke,mesh,ele)
         
         # Adiciona a matriz do elemento (rotacionada) à matriz Global
-        K[gls,gls] .= K[gls,gls] .+ Keg*(x[ele]^p)
+        K[gls,gls] .= K[gls,gls] .+ Keg*kparam(x[ele])
 
     end #ele
 
@@ -48,21 +61,58 @@ function Global_K(mesh::Mesh; x=Float64[], p=1.0)
 
 end
 
+#
+# Version without parametrization
+#
+"""
+Assembly the global stiffness matrix.
+
+    Global_K(mesh::Mesh)
+
+"""
+function Global_K(mesh::Mesh)
+
+    # Dummy function 
+    dummy_f(x)=1.0
+
+    # Call Global_K
+    Global_K(mesh, Float64[], dummy_f)
+end
 
 
- function Global_M(mesh::Mesh; x=Float64[])
+
+"""
+Assembly the global mass matrix.
+
+     Global_M(mesh::Mesh, x=Float64[], mparam::Function)
+
+where mparam(x) can be, for example
+
+function param(x,p=2.0,cut=0.1)
+    s = x
+    if x<cut
+       s = x^p
+    end
+    return s
+end
+
+for a SIMP like material parametrization.
+"""
+ function Global_M(mesh::Mesh, xin::Vector{Float64}, mparam::Function)
 
     # Alias
-    bmesh = mesh.bmesh
-    ne = bmesh.ne
+    ne = Get_ne(mesh)
+    nn = Get_nn(mesh)
 
-    # Basic test for x
-    if isempty(x)
+    # Basic test for xe
+    if isempty(xin)
         x = ones(ne)
+    else
+        x = copy(xin)
     end
     
     # Vamos reforçar a ideia de que x tem a dimensão ne
-    @assert length(x)==bmesh.ne "Global_M::x deve ter dimensão igual o número de elementos"
+    @assert length(x)==ne "Global_M::x deve ter dimensão igual o número de elementos"
 
     # Dimensão do problema
     dim = Get_dim(mesh)
@@ -71,7 +121,7 @@ end
     etype = Get_etype(mesh)
 
     # Primeira coisa é alocar a matriz M 
-    ng = dim*bmesh.nn
+    ng = dim*nn
     M = spzeros(ng,ng)
 
     # Loop pelos elementos, calculando a matriz local Me de cada um
@@ -89,7 +139,7 @@ end
         Meg = To_global(Me,mesh,ele)
         
         # Adiciona a matriz do elemento (rotacionada) a matriz Global
-        M[gls,gls] .= M[gls,gls] .+ Meg*(x[ele])
+        M[gls,gls] .= M[gls,gls] .+ Meg*mparam(x[ele])
 
     end #ele
 
@@ -98,3 +148,87 @@ end
 
 end
 
+"""
+Assembly the global mass matrix.
+
+     Global_M(mesh::Mesh)
+
+"""
+function Global_M(mesh::Mesh)
+
+    # Dummy function
+    dummy_f(x)=1.0
+
+    # Call function
+    Global_M(mesh, Float64[], dummy_f)
+end
+#
+# Evaluate stresses for the entire mesh (central points only)
+#
+"""
+Return stresses for the entire mesh. This version evaluates only in the central point.
+
+  Stresses(mesh::Mesh,U::Vector{T};x=Float64[],sparam::Function)
+
+The output is a matrix ne x ncol, where ncol is 1 for :truss2D and 3D, 3 for :solid2D and 6 for :solid3D
+
+Function sparam(x) is used to parametrize stress. One possibility is 
+
+function sparam(x,p=1.0,q=0.0) 
+       x^(p=q)
+end
+
+"""
+function Stresses(mesh::Mesh,U::Vector{T},xin::Vector{Float64},sparam::Function)  where T
+
+   # Number of elements
+   ne = Get_ne(mesh)
+
+    # Basic test for xe
+    if isempty(xin)
+        x = ones(ne)
+    else
+        x = copy(xin)
+    end
+
+   # Alocate the output array. It depends on the number of stresses 
+   # for each element type
+   etype = Get_etype(mesh)
+
+   ncol = 1
+   if etype==:solid2D
+      ncol = 3
+   elseif etype==:solid3D
+      ncol = 6
+   end
+
+   # Alocate
+   stresses = zeros(ne,ncol)
+
+   # Loop 
+   for ele in mesh
+      s = Stress(mesh,ele,U)
+      stresses[ele,:].=s[:]*sparam(x[ele])
+   end
+
+   # Return stresses
+   return stresses
+
+end
+
+"""
+Return stresses for the entire mesh. This version evaluates only in the central point.
+
+  Stresses(mesh::Mesh,U::Vector{T})
+
+The output is a matrix ne x ncol, where ncol is 1 for :truss2D and 3D, 3 for :solid2D and 6 for :solid3D
+
+"""
+function Stresses(mesh::Mesh,U::Vector{T})  where T
+
+    # Dummy function
+    dummy_f(x)=1.0
+
+    # Call function
+    Stresses(mesh,U,Float64[],dummy_f)
+end
