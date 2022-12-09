d821e87ac3a30eeb2f83e6526467d1920105e6f1
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-12-09 09:08:47 -0300

    Add tests for stress

--- test/test_linear.jl
+++ test/test_linear.jl
@@ -108,3 +108,135 @@ end
     @isinferred Solve_linear(mesh,x,kparam)
 
 end
+
+@testset "Solve_linear - Stresses" begin
+    
+    # Load Simply supported 2D from TMeshes
+    mesh = Simply_supported2D(6,6)
+
+    # Solve the linear static equilibrium
+    @test try  U,_ = Solve_linear(mesh)
+            true
+    catch err  
+            false
+    end
+    @isinferred LFEM.Solve_linear(mesh)
+
+    # Stresses- center
+    @test try sigma = Stresses(mesh,U,center=true)
+             true
+    catch err  
+            false
+    end
+    @test size(sigma,2)==1
+    @isinferred LFEM.Stresses(mesh,U,center=true)
+    
+
+    # Stresses- Gauss points (for bars is the same)
+    @test try sigma = Stresses(mesh,U,center=false)
+        true
+    catch err  
+        false
+    end
+    @test size(sigma,2)==1
+    @isinferred LFEM.Stresses(mesh,U,center=false)
+
+    ####################
+
+    # Load Simply supported 2D from TMeshes (solid)
+    mesh = Simply_supported2D(6,6,:solid2D)
+
+    # Solve the linear static equilibrium
+    @test try  U,_ = Solve_linear(mesh)
+            true
+    catch err  
+            false
+    end
+    @isinferred Solve_linear(mesh)
+
+
+    # Stresses- center
+    @test try sigma = Stresses(mesh,U,center=true)
+        true
+    catch err  
+    false
+    end
+    @test size(sigma,2)==3
+    @isinferred LFEM.Stresses(mesh,U,center=true)
+
+
+    # Stresses- Gauss points 
+    @test try sigma = Stresses(mesh,U,center=false)
+        true
+    catch err  
+        false
+    end
+    @test size(sigma,2)==3*4
+    @isinferred LFEM.Stresses(mesh,U,center=false)
+
+    #######################
+
+    # Load Simply supported 3D from TMeshes (truss)
+    mesh = Simply_supported3D(2,2,2)
+
+    # Solve the linear static equilibrium
+    @test try  U,_  = Solve_linear(mesh)
+        true
+    catch err  
+        false   
+    end
+    @isinferred Solve_linear(mesh)
+
+    # Stresses- center
+    @test try sigma = Stresses(mesh,U,center=true)
+        true
+    catch err  
+        false
+    end
+    @test size(sigma,2)==1
+    @isinferred LFEM.Stresses(mesh,U,center=true)
+
+
+    # Stresses- Gauss points (for bars is the same)
+    @test try sigma = Stresses(mesh,U,center=false)
+        true
+    catch err  
+        false
+    end
+    @test size(sigma,2)==1
+    @isinferred LFEM.Stresses(mesh,U,center=false)
+
+    #######################
+
+    # Load Simply supported 3D from TMeshes (solid)
+    mesh = Simply_supported3D(2,2,2,:solid3D)
+
+    # Solve the linear static equilibrium
+    @test try  U,_  = Solve_linear(mesh)
+        true
+    catch err  
+        false
+    end
+    @isinferred Solve_linear(mesh)
+
+
+    # Stresses- center
+    @test try sigma = Stresses(mesh,U,center=true)
+        true
+    catch err  
+        false
+    end
+    @test size(sigma,2)==6
+    @isinferred LFEM.Stresses(mesh,U,center=true)
+
+
+    # Stresses- Gauss points
+    @test try sigma = Stresses(mesh,U,center=false)
+        true
+    catch err  
+        false
+    end
+    @test size(sigma,2)==6*8
+    @isinferred LFEM.Stresses(mesh,U,center=false)
+
+end
