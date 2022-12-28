13edcc2eb207cf0afdab70b3ed73c191f2aa824c
CodeLenz <eduardobarplenz@gmail.com>
2022-12-27 19:33:31 -0500

    o my...2

--- test/test_arnoldi.jl
+++ test/test_arnoldi.jl
@@ -17,8 +17,8 @@
    
 
     # Randon, larger problem
-    A = rand(100,100) + 100*I(100)
-    B = rand(100,100) + 200*I(100)
+    A = rand(100,100); A = A .+ A' #+ 100*I(100)
+    B = rand(100,100); B = B .+ B' #+ 200*I(100)
     av, AV = Solve_Eigen_(A,B,20)
 
     @test isapprox(norm(A*AV - B*AV*Diagonal(av)),0.0)
