2f8577ae785dbc44cba1c3d5838a08586623a3da
CodeLenz <eduardobarplenz@gmail.com>
2023-01-04 09:11:41 -0500

    Normalize eigenvectors. Export new flag. More tests for orthogonal eigenvectors and for the satisfaction of the modal equation

--- src/solve_arnoldi.jl
+++ src/solve_arnoldi.jl
@@ -32,17 +32,36 @@ nev  -> Number of eigenvalues (2*nev are evaluated to account for possible negat
 positive -> only positive values are returned
 order    -> eigenvalues are sorted in ascending order
 tol and restarts are defined in ArnoldiMethod
+verbose -> if true, show some debug messages
 
 Returns
+flag 
+  1 if OK
+ -1 if partialschur fails
+ -2 if solution is not OK
+ -3 if eigenvectors are not orthogonal
 
 eigenvalues (real vector)
 eigenvectors (real matrix)
 
-"""
-function Solve_Eigen_(A::AbstractMatrix, B::AbstractMatrix, nev=4, positive=true, order=true, tol=1E-5, restarts=500)
+obs: If flag is -1 eigenvalues is zeros(1) and eigenvectors is zeros(1,1).
 
+"""
+function Solve_Eigen_(A::AbstractMatrix, B::AbstractMatrix, nev=4; positive=true, order=true, tol=1E-8, restarts=500, verbose=false)
+
+    # Return flag 
+    #  1 if OK
+    # -1 if partialschur fails
+    # -2 if solution is not OK
+    # -3 if eigenvectors are not orthogonal
+  
     # Target the largest eigenvalues of the inverted problem
     decomp, history  = partialschur(construct_linear_map(A, B), nev=nev, tol=tol, restarts=restarts, which=LM())
+
+    # Test for convergency
+    history.converged  || return -1, zeros(1), zeros(1,1) #throw("Solve_eigen:: $(string(history))")
+     
+    # Extract eigenvalues and eigenvectors
     λs_inv, X = partialeigen(decomp)
 
     # Eigenvalues have to be inverted to find the smallest eigenvalues of the non-inverted problem.
@@ -61,12 +80,73 @@ function Solve_Eigen_(A::AbstractMatrix, B::AbstractMatrix, nev=4, positive=true
         λp = λs
     end
 
+    # Effective number of eigenvalues
+    nλp = length(λp)
+
     if order
        p = sortperm(λp) 
     else
-       p = collect(1:length(λp))
+       p = collect(1:nλp)
     end
         
+    # Alias to sorted solutions
+    sorted_λp = @view λp[p]
+    sorted_X  = real.(X[:,p])
+
+    # Normalize sorted_X[:,i] - just to make it easier to verify the 
+    # eigenvectors in the output of this function
+    for i=1:nλp
+
+        # Extreme values 
+        mn = minimum(sorted_X[:,i])
+        mx = maximum(sorted_X[:,i])
+
+        # Take the absolute value of the most extreme
+        normat = mx
+        if abs(mn)>mx
+            normat = mn
+        end
+
+        sorted_X[:,i]  ./= normat
+    end
+
+
+    # OK, now we have to check if the solution is OK.
+
+    # First, we verify if the solution is correct
+    check1 = norm(A*sorted_X .- B*sorted_X*Diagonal(sorted_λp))
+
+    # If verbose
+    verbose && println("Solve_Eigen_:: check1 is ", check1)
+    
+    # If solution is not OK
+    (check1 < 1) || return -2, sorted_λp, sorted_X   #throw("Solve_Eigen_:: Solution is not OK. Revert to other solver")
+
+    # Second, we check if eigenvectors are orthogonal
+    flag_ortho = true
+    for i=1:nλp
+
+        for j=i:nλp
+            if i!=j
+
+                # Evaluate the dot product between two distinct eigenvectors
+                ortho = abs(dot(sorted_X[:,i],sorted_X[:,j]))
+
+                # If verbose
+                verbose && println("Solve_Eigen_:: check ortho ($i,$j) ", ortho)
+    
+                # Check ortho
+                if ortho >= 1E-1
+                    flag_ortho = false
+                    break
+                end
+            end
+        end
+    end
+    flag_ortho || return -3, sorted_λp, sorted_X #throw("Solve_Eigen_:: eigenvectors are not orthogonal.")
+
+    # It seems that everithing is OK
     # Return both the eigenvalues and the eigenvectors
-    return λp[p], real.(X[:,p])
+    return  1, sorted_λp, sorted_X
+
 end
--- test/test_arnoldi.jl
+++ test/test_arnoldi.jl
@@ -4,8 +4,11 @@
    A = [20.0 0.0 ; 0.0 40.0]
    B = [30.0 0.0 ; 0.0 40.0]
 
-   # Solve 
-   av, AV = Solve_Eigen_(A,B,2)
+   # Solve with our method
+   flag, av, AV = Solve_Eigen_(A,B,2)
+
+   # Test flag
+   @test flag==1
 
    # Test eigenvalues
    @test isapprox(av[1],2/3)
@@ -13,26 +16,41 @@
 
    # Test eigenvectors
    @test isapprox(AV[:,1],[1.0 ; 0.0])
-   @test isapprox(AV[:,2],[0.0 ; -1.0])
+   @test isapprox(AV[:,2],[0.0 ; 1.0])
    
 
-    # Larger problem
-    A = diagm(collect(10:10:100)) .+ rand(10,10)
+    # Larger problem 
+    A = diagm(collect(10:10:np)) .+ rand(10,n10)
     B = diagm(collect(1.0:-0.1:0.1)) 
 
-    av, AV = Solve_Eigen_(A,B,6,false)
+    # Solve with our method
+    nev = 6
+    flag, av, AV = Solve_Eigen_(A,B,nev,positive=false,verbose=true)
+
+    # test flag
+    @test flag==1
 
     @test isapprox(norm(A*AV .- B*AV*Diagonal(av)),0.0,atol=1E-6)
 
-    # Test for ortogonality
-    #orto = Array(qr(AV).Q)
-    #for i=1:20
-    #    for j=1:20
-    #        if i!=j
-    #           @test isapprox(dot(orto[:,i],orto[:,j]),0.0,atol=1E-6)
-    #        end
-    #    end
-    #end
+    # Solve with eigen
+    aref, vref = eigen(A,B)
+
+    # Test for eigenvalues
+    for i=1:nev
+        @test isapprox(aref[i],av[i])
+    end
+
+    #
+    # Known problem - Arnlodi is not working with this problem
+    #
+
+    A = 2.0.*I(10)
+    B = 5.0.*I(10)
+    flag, av, AV = Solve_Eigen_(A,B,2)
+
+    @test flag == -1
+    @test av == zeros(1)
+    @test AV == zeros(1,1)
 
 
 end
