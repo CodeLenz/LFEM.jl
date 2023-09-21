34ca8c0792b2ba059778b981e447358a8f7159d0
Eduardo Lenz <eduardobarplenz@gmail.com>
2023-09-21 11:18:06 -0300

    Update linear.jl

--- src/analysis/linear.jl
+++ src/analysis/linear.jl
@@ -37,7 +37,7 @@ function Solve_linear(mesh::Mesh, x::Vector{Float64}, kparam::Function; loadcase
     Kv = K[free_dofs,free_dofs]
 
     # Create LinearSolve problem
-    prob = LinearProblem(Symmetric(Kv),F[free_dofs])
+    prob = LinearProblem((Kv),F[free_dofs])
     linsolve = init(prob)
 
     @show "solve" 
