fc1bb5223b6294795c9464c33263e1f296fa2a52
Eduardo Lenz <eduardobarplenz@gmail.com>
2024-03-06 19:07:06 -0300

    use cache for linsolve

--- src/analysis/newmark.jl
+++ src/analysis/newmark.jl
@@ -203,8 +203,8 @@ function Solve_newmark(mesh::Mesh, f!::Function, gls::Matrix{Int64},
         # R.H.S in t+dt
         #b .= F .- K*U0 .-(C .+Δt*K)*V0 .- (C*Δt*(1-γ) .+ K*(1/2-β)*Δt^2)*A0
             
-        b .= F[free_dofs] .- sKv*U0[free_dofs] .- CKdt*V0[free_dofs] .- CKbeta*A0[free_dofs]
-        linsolve = LinearSolve.set_b(linsolve,b)   
+        linsolve.b .= F[free_dofs] .- sKv*U0[free_dofs] .- CKdt*V0[free_dofs] .- CKbeta*A0[free_dofs]
+        #linsolve = LinearSolve.set_b(linsolve,b)   
 
         # Solve for A in t+Δt
         sol = solve!(linsolve)
