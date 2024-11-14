f06fc7b2d2589d15ea95e31aa36e8f9987217605
Eduardo Lenz <eduardobarplenz@gmail.com>
2024-11-14 11:56:42 -0300

    Export gmsh_nogal_stresses

--- src/LFEM.jl
+++ src/LFEM.jl
@@ -54,5 +54,6 @@ module LFEM
        export Harmonic_stresses, Voigt_equivalent
        export B_element
        export Nodal_stress_smooth, Global_stress_smooth
+       export Gmsh_nodal_stresses
 
 end # module
